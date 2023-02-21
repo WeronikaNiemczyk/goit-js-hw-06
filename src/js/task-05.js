dfd571a084c3ae4362b80daddb223d25f0eb86ff
Weronika Niemczyk <weronika.tlusciak@gmail.com>
2023-02-21 16:52:09 +0100

    Task 5 - correct

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -2,7 +2,11 @@ const textInput = document.querySelector(`#name-input`);
 const textOutput = document.querySelector(`#name-output`);
 
 const showText = (event) => {
+  if (textInput.value !== "") {
     textOutput.textContent = event.currentTarget.value;
-}
+  } else {
+    textOutput.textContent = "Anonymous";
+  }
+};
 
 textInput.addEventListener("input", showText);
